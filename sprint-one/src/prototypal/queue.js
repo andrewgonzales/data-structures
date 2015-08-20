09adbd4cb2e594b2cc4da238700438a14de60714
andrewgonzales <andrewmartingonzales@gmail.com>
2015-08-19 21:42:00 -0700

    Completed prototypal and pseudoclassical patterns for queue and stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,41 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+
+  var total  = Object.create(queueMethods);
+  total.count = 0;
+  total.storage = storage;
+
+  
+  return total;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+  this.storage[this.count] = value;
+  this.count++;
+};
 
+queueMethods.dequeue = function(){
+  var temp = this.storage['0'];
+  delete this.storage['0'];
+  this.count--;
+  if(this.count < 0){
+    this.count = 0;
+  }
+
+  for(var key in this.storage) {
+    for(var i = 0; i < this.count; i++){
+      this.storage[i] = this.storage[key];
+      delete this.storage[key];
+    }
+  }
+
+  return temp;
+};
+
+queueMethods.size = function(){
+  return this.count;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,40 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  
+
+  var total = Object.create(stackMethods);
+  total.storage = storage;
+  total.count = 0;
+  
+  
+  return total;
 };
 
 var stackMethods = {};
 
 
+stackMethods.push = function(value) {
+  
+  this.storage[this.count] = value;
+  this.count++;
+  
+  return this.storage;
+};
+
+stackMethods.pop = function() {
+  var temp;
+  this.count--;
+  if (this.count === -1) {
+    this.count = 0;
+  }
+  temp = this.storage[this.count];
+  delete this.storage[this.count];
+
+  return temp;
+};
+
+stackMethods.size = function() {
+  return this.count;
+};

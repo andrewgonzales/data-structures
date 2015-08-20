0ca109fd53ba411012ce0dbeb5fafc9513e09f64
andrewgonzales <andrewmartingonzales@gmail.com>
2015-08-20 15:34:21 -0700

    Completed Linked List along with complexity question

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,46 @@
 var LinkedList = function(){
+
+
   var list = {};
   list.head = null;
   list.tail = null;
 
+  
+
   list.addToTail = function(value){
+
+    if (this.head === null) {
+      var temp = new Node(value);
+      temp.next = this.head;
+      this.head = temp;
+      this.tail = temp;
+    }
+    else {
+      var temp = new Node(value);
+      this.tail.next = temp;
+      this.tail = temp;
+    }
   };
 
   list.removeHead = function(){
+    var tempValue = this.head.value;
+    var tempNext = this.head.next;
+    delete this.head;
+    this.head = tempNext;
+  
+    return tempValue;
   };
 
   list.contains = function(target){
+    var node = this.head;
+    var found = this.tail.value === target;
+    while(node.next !== null){
+      if(node.value === target){
+        found = true;
+      } 
+      node = node.next;
+    }
+    return found; 
   };
 
   return list;
@@ -24,6 +55,8 @@ var Node = function(value){
   return node;
 };
 
+
 /*
  * Complexity: What is the time complexity of the above functions?
+ *addToTail and removeHead are O(1), but contains is O(n)
  */

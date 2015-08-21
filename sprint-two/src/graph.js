50b41e10ce746e9432f58b8d870c076ed6e5b5b1
andrewgonzales <andrewmartingonzales@gmail.com>
2015-08-21 16:03:48 -0700

    Completed Graph and Set and answered Complexity questions

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -4,45 +4,130 @@
 
 // Instantiate a new graph
 var Graph = function(){
+  this.nodeStorage = [];
+
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node){
+  var graphNode = {};
+  graphNode.value = node;
+  graphNode.edges = [];
+
+  this.nodeStorage.push(graphNode);
+
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node){
+Graph.prototype.contains = function(value){
+  var found = false;
+  for (var i = 0; i < this.nodeStorage.length; i++) {
+    if (this.nodeStorage[i].value === value) {
+      found = true;
+      break;
+    }
+    else {
+      found = false;
+    }
+  }
+
+  return found;
 };
 
 // ------------------------
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node){
+Graph.prototype.removeNode = function(value){
+
+  for(var i = 0; i < this.nodeStorage.length; i++) {
+    if(this.nodeStorage[i].value === value) {
+      this.nodeStorage.splice(i,1);
+    }
+  }
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  var found = false;
+
+  for(var i = 0; i < this.nodeStorage.length; i++){
+    if(this.nodeStorage[i].value === fromNode){
+      var from = i;
+    }
+  }
+
+  for(var i = 0; i < this.nodeStorage[from].edges.length; i++){
+    found = this.nodeStorage[this.nodeStorage[from].edges[i]].value === toNode;
+    if(found){
+      break;
+    }
+  }
+
+  return found;
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode){
+
+  for(var i = 0; i < this.nodeStorage.length; i++){
+
+    if(this.nodeStorage[i].value === fromNode){
+      var from = i;
+    }
+    if(this.nodeStorage[i].value === toNode){
+      var to = i;
+    }
+  }  
+
+  if(from !== undefined && to !== undefined){
+
+    this.nodeStorage[from].edges.push(to);
+    this.nodeStorage[to].edges.push(from);
+  }
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  for(var i = 0; i < this.nodeStorage.length; i++){
+    if(this.nodeStorage[i].value === fromNode){
+      var from = i;
+    }
+    if(this.nodeStorage[i].value === toNode){
+      var to = i;
+    }
+  }
+
+  for(var i = 0; i < this.nodeStorage[from].edges.length; i++){
+    var foundToIndex = this.nodeStorage[this.nodeStorage[from].edges[i]].value === toNode;
+    if(foundToIndex){
+      this.nodeStorage[from].edges.splice(i,1);
+      var foundFromIndex = this.nodeStorage[to].edges.indexOf(from);
+      this.nodeStorage[to].edges.splice(foundFromIndex,1);
+    }
+  }
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb){
+  for(var i = 0; i < this.nodeStorage.length; i++) {
+    cb(this.nodeStorage[i].value);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addNode is O(1)
+ * contains is O(n)
+ * removeNode is O(n)
+ * hasEdge is O(n)
+ * addEdge is O(n)
+ * removeEdge is O(n)
+ * forEachNode is O(n)
  */
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,27 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set.storage = {};
+  set.count = 0;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+
+  this.storage[item] = this.count;
+  this.count++;
 };
 
 setPrototype.contains = function(item){
+  return (item in this.storage);    
 };
 
 setPrototype.remove = function(item){
+  delete this.storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * all functions have a time complexity of O(1).
  */

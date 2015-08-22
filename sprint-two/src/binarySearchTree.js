4337481a21e3a1d17cb1bd7ac1f36da02bfe32f2
andrewgonzales <andrewmartingonzales@gmail.com>
2015-08-21 22:58:49 -0700

    Completed insert and contains for binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,63 @@
 var BinarySearchTree = function(value){
+  var newBinaryTree = {};
+  newBinaryTree.value = value;
+  newBinaryTree.left = null;
+  newBinaryTree.right = null;
+
+  _.extend(newBinaryTree, BinarySearchTreeMethods);
+
+  return newBinaryTree;
+
+};
+
+var BinarySearchTreeMethods = {};
+
+BinarySearchTreeMethods.insert = function(value){
+    
+  if(value < this.value){
+
+    if(this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else { //for when value > this.value
+
+    if(this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+BinarySearchTreeMethods.contains = function(value){
+
+  var found;
+
+
+  if(value < this.value){
+    if(this.left === null) {
+      found = false;
+    } else {
+      found = this.left.contains(value);
+    }
+  } else { //for when value > this.value
+    if(this.right === null) {
+      found = false;
+    } else {
+      found = this.right.contains(value);
+    }
+  }
+
+  if(value === this.value){
+    found = true;
+  }
+  return found;
+};
+
+BinarySearchTreeMethods.depthFirstLog = function(cb){
+
 };
 
 
